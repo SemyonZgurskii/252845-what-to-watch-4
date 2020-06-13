3967a210c0c3bfcbe6f253a910c078df56c8a91d
Semyon <saymon221090@gmail.com>
2020-06-13 20:16:25 +0300

    Создаёт моковый массив с названиями фильмов. Реализует рендер фильмов опираясь на данный массив. Вводит проверку типов для пропсов компонента main

--- src/index.js
+++ src/index.js
@@ -6,10 +6,15 @@ import App from './components/app/app.jsx';
 const promoInfo = {
   title: `The Grand Budapest Hotel`,
   genre: `Drama`,
-  releaseDate: `2014`
+  releaseDate: 2014
 };
 
+const moviesNames = [`Fantastic Beasts: The Crimes of Grindelwald`, `Bohemian Rhapsody`, `Macbeth`, `Aviator`, `We need to talk about Kevin`, `We need to talk about Kevin`, `Revenant`, `Johnny English`, `Shutter Island`, `Pulp Fiction`, `No Country for Old Men`, `Snatch`, `Moonrise Kingdom`, `Seven Years in Tibet`, `Midnight Special`, `War of the Worlds`, `Dardjeeling Limited`, `Orlando`, `Mindhunter`, `Midnight Special`];
+
 ReactDOM.render(
-    <App {...promoInfo} />,
+    <App
+      promoInfo = {promoInfo}
+      moviesNames = {moviesNames}
+    />,
     document.querySelector(`#root`)
 );
