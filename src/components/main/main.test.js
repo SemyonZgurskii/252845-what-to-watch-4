ea0c2c2a8525b8001fbf876050979a151223381e
Semyon <saymon221090@gmail.com>
2020-06-17 08:57:45 +0300

    Создаёт снепшот тест для Main

--- src/components/main/main.test.js
+++ src/components/main/main.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import renderer from "react-test-renderer";
-import App from './app.jsx';
+import renderer from 'react-test-renderer';
+import Main from './main.jsx';
 
 const promoInfo = {
   title: `The Grand Budapest Hotel`,
@@ -10,17 +10,17 @@ const promoInfo = {
 
 const moviesNames = [`Fantastic Beasts: The Crimes of Grindelwald`, `Bohemian Rhapsody`, `Macbeth`, `Aviator`, `We need to talk about Kevin`, `We need to talk about Kevin`, `Revenant`, `Johnny English`, `Shutter Island`, `Pulp Fiction`, `No Country for Old Men`, `Snatch`, `Moonrise Kingdom`, `Seven Years in Tibet`, `Midnight Special`, `War of the Worlds`, `Dardjeeling Limited`, `Orlando`, `Mindhunter`, `Midnight Special`];
 
-describe(`Create App snapshot`, () => {
-  it(`Should correctly render App`, () => {
-    const AppElement = renderer
+describe(`Create Main snapshot`, () => {
+  it(`Should correctly render Main`, () => {
+    const MainElement = renderer
       .create(
-          <App
+          <Main
             promoInfo={promoInfo}
             moviesNames={moviesNames}
-          />)
+          />
+      )
       .toJSON();
 
-    expect(AppElement).toMatchSnapshot();
+    expect(MainElement).toMatchSnapshot();
   });
 });
-
