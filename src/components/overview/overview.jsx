69e2e23cb0adedd27d6e9174adf93d555f0d6350
Semyon <saymon221090@gmail.com>
2020-07-17 23:04:16 +0300

    Создаёт компонент описания фильма - Overview

---
+++ src/components/overview/overview.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Overview(props) {
+  const {rating, info} = props.movieData;
+  const {score: ratingScore, level: ratingLevel, count: ratingCount} = rating;
+  const {description, director, stars} = info;
+
+  return (
+    <>
+      <div className="movie-rating">
+        <div className="movie-rating__score">{ratingScore}</div>
+        <p className="movie-rating__meta">
+          <span className="movie-rating__level">{ratingLevel}</span>
+          <span className="movie-rating__count">{ratingCount}</span>
+        </p>
+      </div>
+
+      <div className="movie-card__text">
+        <p>{description}</p>
+
+        <p className="movie-card__director"><strong>Director: {director}</strong></p>
+
+        <p className="movie-card__starring"><strong>Starring: {stars.join(`, `)}</strong></p>
+      </div>
+    </>
+  );
+}
+
+Overview.propTypes = {
+  movieData: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    images: PropTypes.shape({
+      card: PropTypes.string.isRequired,
+      smallPoster: PropTypes.string.isRequired,
+      bigPoster: PropTypes.string.isRequired,
+    }),
+    genre: PropTypes.string.isRequired,
+    releaseDate: PropTypes.number.isRequired,
+    rating: PropTypes.shape({
+      score: PropTypes.number.isRequired,
+      level: PropTypes.string.isRequired,
+      count: PropTypes.number.isRequired,
+    }),
+    info: PropTypes.shape({
+      description: PropTypes.string.isRequired,
+      director: PropTypes.string.isRequired,
+      stars: PropTypes.arrayOf(PropTypes.string),
+    }),
+  }),
+};
+
+export default Overview;
