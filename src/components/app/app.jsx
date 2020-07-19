c3093a2a36428103726704a9361b6dbd8cd79d58
Semyon <saymon221090@gmail.com>
2020-07-19 19:57:10 +0300

    рефакторит proptypes для app

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -68,31 +68,8 @@ class App extends PureComponent {
 }
 
 App.propTypes = {
-  moviesData: PropTypes.arrayOf(PropTypes.shape({
-    title: PropTypes.string.isRequired,
-    images: PropTypes.shape({
-      card: PropTypes.string.isRequired,
-      smallPoster: PropTypes.string.isRequired,
-      bigPoster: PropTypes.string.isRequired,
-    }),
-    genre: PropTypes.string.isRequired,
-    releaseDate: PropTypes.number.isRequired,
-    rating: PropTypes.shape({
-      score: PropTypes.number.isRequired,
-      level: PropTypes.string.isRequired,
-      count: PropTypes.number.isRequired,
-    }),
-    info: PropTypes.shape({
-      description: PropTypes.string.isRequired,
-      director: PropTypes.string.isRequired,
-      stars: PropTypes.arrayOf(PropTypes.string),
-    }),
-  })),
-  promoInfo: PropTypes.shape({
-    title: PropTypes.string.isRequired,
-    genre: PropTypes.string.isRequired,
-    releaseDate: PropTypes.number.isRequired,
-  }),
+  moviesData: PropTypes.arrayOf(PropTypes.object),
+  promoInfo: PropTypes.object,
   onMovieTitleClick: PropTypes.func.isRequired,
 };
 
