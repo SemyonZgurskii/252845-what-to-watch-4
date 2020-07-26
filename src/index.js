3feb26152ff1d3df6107ebb047121b7b7c96e3da
Semyon <saymon221090@gmail.com>
2020-07-26 07:58:21 +0300

    подключает store к App

--- src/index.js
+++ src/index.js
@@ -3,14 +3,24 @@ import ReactDOM from 'react-dom';
 
 import App from './components/app/app.jsx';
 import {moviesData, promoInfo} from './mocks/movies.js';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import {reducer} from './reducer.js';
 
 const onMovieTitleClick = () => {};
 
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+);
+
 ReactDOM.render(
-    <App
-      promoInfo = {promoInfo}
-      moviesData = {moviesData}
-      onMovieTitleClick = {onMovieTitleClick}
-    />,
+    <Provider store={store}>
+      <App
+        promoInfo = {promoInfo}
+        moviesData = {moviesData}
+        onMovieTitleClick = {onMovieTitleClick}
+      />
+    </Provider>,
     document.querySelector(`#root`)
 );
