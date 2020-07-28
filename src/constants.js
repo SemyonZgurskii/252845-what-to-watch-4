3be638e5c02431f98505b4f0d165a1cb9ccd69a3
Semyon <saymon221090@gmail.com>
2020-07-28 22:10:22 +0300

    создаёт файл для хранения констант - constants.js

---
+++ src/constants.js
@@ -0,0 +1,12 @@
+export const Genre = {
+  ALL: `All genres`,
+  COMEDIES: `Comedies`,
+  CRIME: `Crime`,
+  DOCUMENTARY: `Documentary`,
+  DRAMAS: `Dramas`,
+  HORROR: `Horror`,
+  KIDS_AND_FIMILY: `Kids & Family`,
+  ROMANCE: `Romance`,
+  SCI_FI: `Sci-Fi`,
+  THRILLERS: `Thrillers`,
+};
