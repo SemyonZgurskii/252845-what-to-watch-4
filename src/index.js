a4d3b77cfe28b4bd6bcb594c67672fb8e67c8f2f
Semyon <saymon221090@gmail.com>
2020-07-28 22:11:08 +0300

    удаляет лишние пропсы в index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './components/app/app.jsx';
-import {moviesData, promoInfo} from './mocks/movies.js';
+import {promoInfo} from './mocks/movies.js';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 import {reducer} from './reducer.js';
@@ -18,7 +18,6 @@ ReactDOM.render(
     <Provider store={store}>
       <App
         promoInfo = {promoInfo}
-        moviesData = {moviesData}
         onMovieTitleClick = {onMovieTitleClick}
       />
     </Provider>,
