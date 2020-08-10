df36af724bacdc8a1e579e613a332e746542e795
Semyon <saymon221090@gmail.com>
2020-08-10 22:52:09 +0300

    удаляет состояние из MoviesList. Делает из него функциональный компонент

--- src/components/movies-list/movies-list.jsx
+++ src/components/movies-list/movies-list.jsx
@@ -1,78 +1,40 @@
-import React, {PureComponent} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import MovieCard from '../movie-card/movie-card.jsx';
-import ShowMore from '../show-more/show-more.jsx';
 import withVideo from '../../hocs/with-video/with-video.jsx';
 
-const MOVIES_COUNT_SUMMAND = 8;
 const MovieCardWrapped = withVideo(MovieCard);
 
-function getShowedMoviesCount(currentMoviesCount, allMoviesCount) {
-  if (currentMoviesCount + MOVIES_COUNT_SUMMAND > allMoviesCount) {
-    return allMoviesCount;
-  }
+function MoviesList(props) {
+  const {moviesData, onMovieTitleClick, onCardClick, children} = props;
 
-  return currentMoviesCount + MOVIES_COUNT_SUMMAND;
-}
-
-class MoviesList extends PureComponent {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      showedMoviesCount: getShowedMoviesCount(0, this.props.moviesData.length),
-    };
-
-    this._handleShowMoreButtonClick = this._handleShowMoreButtonClick.bind(this);
-  }
-
-  componentWillUnmount() {
-    clearTimeout(this.timerID);
-  }
-
-  _handleShowMoreButtonClick() {
-    this.setState((prevState) => {
-      const {moviesData} = this.props;
-      const {showedMoviesCount} = prevState;
-
-      return {
-        showedMoviesCount: getShowedMoviesCount(showedMoviesCount, moviesData.length)
-      };
-    });
-  }
-
-  render() {
-    const {moviesData, onMovieTitleClick, onCardClick} = this.props;
-    const showedMovies = moviesData.slice(0, this.state.showedMoviesCount);
-
-    return (
-      <>
-        <div className="catalog__movies-list">
-          {showedMovies.map((movieData, index) => {
-            return (
-              <MovieCardWrapped
-                movieData={movieData}
-                onMovieTitleClick={onMovieTitleClick}
-                onCardClick={onCardClick}
-                key={movieData.title + index}
-              />
-            );
-          })}
-        </div>
-        {this.state.showedMoviesCount !== this.props.moviesData.length &&
-          <ShowMore
-            onClick={this._handleShowMoreButtonClick}
-          />
-        }
-      </>
-    );
-  }
+  return (
+    <>
+      <div className="catalog__movies-list">
+        {moviesData.map((movieData, index) => {
+          return (
+            <MovieCardWrapped
+              movieData={movieData}
+              onMovieTitleClick={onMovieTitleClick}
+              onCardClick={onCardClick}
+              key={movieData.title + index}
+            />
+          );
+        })}
+      </div>
+      {children}
+    </>
+  );
 }
 
 MoviesList.propTypes = {
   moviesData: PropTypes.array.isRequired,
   onMovieTitleClick: PropTypes.func.isRequired,
-  onCardClick: PropTypes.func.isRequired
+  onCardClick: PropTypes.func.isRequired,
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ]).isRequired,
 };
 
 export default MoviesList;
